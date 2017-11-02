f376a791ea1175376777b0dc8846939ece1de256
Daniele Conventi <conventi@hastega.it>
2017-11-02 22:41:22 +0100

    Cleaned the code with Go fmt.

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"github.com/tockins/fresh"
-	"net/http"
 	"github.com/tockins/fresh-examples/models"
+	"net/http"
 )
 
 func main() {
@@ -11,13 +11,13 @@ func main() {
 	f.Config().SetPort(8080)
 
 	// API definition with path and related controller
-	f.GET("/todos/", func(c fresh.Context) error{
-		todos := [] *models.Todo{}
+	f.GET("/todos/", func(c fresh.Context) error {
+		todos := []*models.Todo{}
 		todos = append(todos, &models.Todo{})
 		todos = append(todos, &models.Todo{})
 		return c.Response().JSON(http.StatusOK, todos)
 	})
-	f.GET("/todos/:uuid", func(c fresh.Context) error{
+	f.GET("/todos/:uuid", func(c fresh.Context) error {
 		todo := models.Todo{c.Request().URLParam("uuid"), "First Todo", "done"}
 		return c.Response().JSON(http.StatusOK, todo)
 	})
