d9d80b973b8b1fbd05778f861e22c70af31b2626
Daniele Conventi <conventi@hastega.it>
2017-11-03 13:40:13 +0100

    Cleaned the code with go fmt.

--- models/todo.go
+++ models/todo.go
@@ -1,7 +1,7 @@
 package models
 
 type Todo struct {
-	Uuid string  `json:"uuid"`
-	Title string `json:"title"`
+	Uuid   string `json:"uuid"`
+	Title  string `json:"title"`
 	Status string `json:"status"`
 }
